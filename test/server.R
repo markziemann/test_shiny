837880eae54704902444f293c0e89689315546c6
markziemann <mark.ziemann@gmail.com>
2020-08-30 23:16:37 +1000

    t-test

--- test/server.R
+++ test/server.R
@@ -11,6 +11,7 @@ server <- function(input, output, session) {
         myvec <- strsplit(newdata,"\n")[[1]]
         myvec <- as.numeric(myvec)
         median1 <- median(myvec)
+        median1 <- round(median1,3)
         paste0(median1)
     })
     output$median1 <- renderText(median1())
@@ -20,6 +21,7 @@ server <- function(input, output, session) {
         myvec <- strsplit(newdata,"\n")[[1]]
         myvec <- as.numeric(myvec)
         mean1 <- mean(myvec)
+        mean1 <- round(mean1,3)
         paste0(mean1)
     })
     output$mean1 <- renderText(mean1())
@@ -29,10 +31,66 @@ server <- function(input, output, session) {
         myvec <- strsplit(newdata,"\n")[[1]]
         myvec <- as.numeric(myvec)
         sd1 <- sd(myvec)
+        sd1 <- round(sd1,3)
         paste0(sd1)
     })
     output$sd1 <- renderText(sd1())
 
+    median2 <- eventReactive(input$go, {
+      newdata <-  as.character(input$set2)
+      myvec <- strsplit(newdata,"\n")[[1]]
+      myvec <- as.numeric(myvec)
+      median2 <- median(myvec)
+      median2 <- round(median2,3)
+      paste0(median2)
+    })
+    output$median2 <- renderText(median2())
+    
+    mean2 <- eventReactive(input$go, {
+      newdata <-  as.character(input$set2)
+      myvec <- strsplit(newdata,"\n")[[1]]
+      myvec <- as.numeric(myvec)
+      mean2 <- mean(myvec)
+      mean2 <- round(mean2,3)
+      paste0(mean2)
+    })
+    output$mean2 <- renderText(mean2())
+    
+    sd2 <- eventReactive(input$go, {
+      newdata <-  as.character(input$set2)
+      myvec <- strsplit(newdata,"\n")[[1]]
+      myvec <- as.numeric(myvec)
+      sd2 <- sd(myvec)
+      sd2 <- round(sd2,3)
+      paste0(sd2)
+    })
+    output$sd2 <- renderText(sd2())
+
+    myt <- eventReactive(input$go, {
+      newdata1 <-  as.character(input$set1)
+      myvec <- strsplit(newdata1,"\n")[[1]]
+      myvec1 <- as.numeric(myvec)
+      newdata2 <-  as.character(input$set2)
+      myvec <- strsplit(newdata2,"\n")[[1]]
+      myvec2 <- as.numeric(myvec)
+      x <- t.test(myvec2,myvec1)
+      myt <- round(x$statistic,3)
+      paste0(myt)
+    })
+    output$myt = renderText(myt())
+    
+    myp <- eventReactive(input$go, {
+      newdata1 <-  as.character(input$set1)
+      myvec <- strsplit(newdata1,"\n")[[1]]
+      myvec1 <- as.numeric(myvec)
+      newdata2 <-  as.character(input$set2)
+      myvec <- strsplit(newdata2,"\n")[[1]]
+      myvec2 <- as.numeric(myvec)
+      x <- t.test(myvec2,myvec1)
+      myp <- round(x$p.value,3)
+      paste0(myp)
+    })    
+    output$myp = renderText(myp())
 }
     
 
--- test/ui.R
+++ test/ui.R
@@ -8,7 +8,11 @@ sidebar <- dashboardSidebar(
                                "Set 1 values",
                                "",
                                height = 200),
-                                  div(style="display: inline-block;vertical-align:top; width: 100px;",
+                 textAreaInput("set2",
+                               "Set 2 values",
+                               "",
+                               height = 200),
+                 div(style="display: inline-block;vertical-align:top; width: 100px;",
                      actionButton("go", "Calculate!")),
                  div(style="display: inline-block;vertical-align:top; width: 100px;",
                      actionButton("reset", "Clear", style='padding:6px;width:80px'))
@@ -17,22 +21,52 @@ sidebar <- dashboardSidebar(
 )
 
 body <- dashboardBody(
-    h2("Set 1  median is: "),
+    h3("Set 1  median is: "),
     h3(verbatimTextOutput("median1", placeholder = T)),
-    h2("Set 1 mean is: "),
+    h3("Set 1 mean is: "),
     h3(verbatimTextOutput("mean1", placeholder = T)),
-    h2("Set 1 standard deviation is: "),
+    h3("Set 1 standard deviation is: "),
     h3(verbatimTextOutput("sd1", placeholder = T)),
     tags$head(tags$style("#median1{color: black;
-                                 font-size: 20px;
+                                 font-size: 15px;
                                  font-family: Source Sans Pro
                                  }")),
     tags$head(tags$style("#mean1{color: black;
-                                 font-size: 20px;
+                                 font-size: 15px;
                                  font-family: Source Sans Pro
                                  }")),
     tags$head(tags$style("#sd1{color: black;
-                                 font-size: 20px;
+                                 font-size: 15px;
+                                 font-family: Source Sans Pro
+                                 }")),
+    h3("Set 2  median is: "),
+    h3(verbatimTextOutput("median2", placeholder = T)),
+    h3("Set 2 mean is: "),
+    h3(verbatimTextOutput("mean2", placeholder = T)),
+    h3("Set 2 standard deviation is: "),
+    h3(verbatimTextOutput("sd2", placeholder = T)),
+    tags$head(tags$style("#median2{color: black;
+                                 font-size: 15px;
+                                 font-family: Source Sans Pro
+                                 }")),
+    tags$head(tags$style("#mean2{color: black;
+                                 font-size: 15px;
+                                 font-family: Source Sans Pro
+                                 }")),
+    tags$head(tags$style("#sd2{color: black;
+                                 font-size: 15px;
+                                 font-family: Source Sans Pro
+                                 }")),
+    h3("t statistic: "),
+    h3(verbatimTextOutput("myt", placeholder = T)),
+    tags$head(tags$style("#myt{color: black;
+                                 font-size: 15px;
+                                 font-family: Source Sans Pro
+                                 }")),
+    h3("p-value: "),
+    h3(verbatimTextOutput("myp", placeholder = T)),
+    tags$head(tags$style("#myp{color: black;
+                                 font-size: 15px;
                                  font-family: Source Sans Pro
                                  }")),
     fluidRow(
